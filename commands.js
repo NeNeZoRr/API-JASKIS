777b433d7b5f7eb0939056fbf089b1a90d1eb1c9
NeNeZoRr <zorrillalin@gmail.com>
2023-11-01 16:45:24 -0400

    Complete

--- commands.js
+++ commands.js
@@ -3,7 +3,8 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
- use jaskis
+
+// use jaskis
 
 // 2. Create a collection called bounties
 db.createCollection('bounties')
@@ -80,22 +81,30 @@ db.bounties.insertMany(
         "captured": false
     }
 )
+
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
 db.bounties.find({ location: "Grasslands" })
+
 // 2. Query for all bounties with a reward worth 10000 or more
 db.bounties.find({ reward: { $gte: 10000 } })
 
 // 3. Query for all bounties, but exclude the client attribute from being shown
+db.bounties.find({}, { client: 0 })
 
 // 4. Query for a Groundhog in the Woodlands
+db.bounties.find({ name: "Groundhog", location: "Woodlands" })
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.updateOne({ name: "Polarwind" }, { $set: { reward: 10000 } })
 
 // 2. Remove Lokinkajou
+db.bounties.deleteOne({ name: "Lokinkajou" })
 
 // 3. Delete all bounties sent by Songbird
+db.bounties.deleteMany({ client: "Songbird" })
 
 // 4. Update all captured statuses to true
+db.bounties.updateMany({}, { $set: { captured: true } })
